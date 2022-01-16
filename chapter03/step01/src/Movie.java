0525ae44ed3aee026777a657f52252065f2f1df4
junseokieo <ccobaltbluee@gmail.com>
2022-01-16 14:27:09 +0900

    04장

---
+++ chapter03/step01/src/Movie.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class Movie {
+    private String title;
+    private Duration runningTime;
+    private Money fee;
+    private List<DiscountCondition> discountConditions;
+
+    private MovieType movieType;
+    private Money discountAmount;
+    private double discountPercent;
+}
+
