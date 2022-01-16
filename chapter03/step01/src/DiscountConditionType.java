0dd4425a81362517cc3d9b25d2b967bc83d765f5
junseokieo <ccobaltbluee@gmail.com>
2022-01-16 17:46:43 +0900

    04장 03절까지

---
+++ chapter03/step01/src/DiscountConditionType.java
@@ -0,0 +1,5 @@
+// 할인 조건의 타입을 결정
+public enum DiscountConditionType {
+    SEQUENCE, // 순번 조건
+    PERIOD // 기간 조건
+}
--- chapter03/step01/src/Movie.java
+++ chapter03/step01/src/Movie.java
@@ -1,13 +1,54 @@
+import java.util.Collections;
 import java.util.List;
 
 public class Movie {
     private String title;
     private Duration runningTime;
     private Money fee;
-    private List<DiscountCondition> discountConditions;
+    private List<DiscountCondition> discountConditions; // 할인 조건의 목록
 
-    private MovieType movieType;
-    private Money discountAmount;
-    private double discountPercent;
+    private MovieType movieType; // 할인 정책의 종류 결정
+    private Money discountAmount; // 할인 조건 직접 정의
+    private double discountPercent; // 할인 조건 직접 정의
+
+    public MovieType getMovieType() {
+        return movieType;
+    }
+
+    public void setMovieType(MovieType movieType) {
+        this.movieType = movieType;
+    }
+
+    public Money getFee() {
+        return fee;
+    }
+
+    public void setFee(Money fee) {
+        this.fee = fee;
+    }
+
+    public List<DiscountCondition> getDiscountConditions() {
+        return Collections.unmodifiableList(discountConditions);
+    }
+
+    public void setDiscountConditions(List<DiscountCondition> discountConditions) {
+        this.discountConditions = discountConditions;
+    }
+
+    public Money getDiscountAmount() {
+        return discountAmount;
+    }
+
+    public void setDiscountAmount(Money discountAmount) {
+        this.discountAmount = discountAmount;
+    }
+
+    public double getDiscountPercent() {
+        return discountPercent;
+    }
+
+    public void setDiscountPercent(double discountPercent) {
+        this.discountPercent = discountPercent;
+    }
 }
 
